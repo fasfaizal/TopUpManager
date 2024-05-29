467b5c184d6b6c9048619992aaf50998132bfef8
Faizal <fas.fazal@gmail.com>
2024-05-29 21:45:10 +0530

    chore: Add initial EF migration

--- TopUpManager.Common/Entity/TopUpTransaction.cs
+++ TopUpManager.Common/Entity/TopUpTransaction.cs
@@ -1,9 +1,12 @@
-﻿namespace TopUpManager.Common.Entity
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TopUpManager.Common.Entity
 {
     public class TopUpTransaction
     {
         public int Id { get; set; }
         public int BeneficiaryId { get; set; }
+        [Column(TypeName = "decimal(5,2)")]
         public decimal Amount { get; set; }
         public DateTime Date { get; set; }
         public Beneficiary Beneficiary { get; set; }
---
+++ TopUpManager.DataAccess/Migrations/20240529160219_Initial.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TopUpManager.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsVerified = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Beneficiaries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nickname = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Beneficiaries", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Beneficiaries_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TopUpTransactions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BeneficiaryId = table.Column<int>(type: "int", nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(5,2)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TopUpTransactions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TopUpTransactions_Beneficiaries_BeneficiaryId",
+                        column: x => x.BeneficiaryId,
+                        principalTable: "Beneficiaries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Beneficiaries_UserId",
+                table: "Beneficiaries",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TopUpTransactions_BeneficiaryId",
+                table: "TopUpTransactions",
+                column: "BeneficiaryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TopUpTransactions");
+
+            migrationBuilder.DropTable(
+                name: "Beneficiaries");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}

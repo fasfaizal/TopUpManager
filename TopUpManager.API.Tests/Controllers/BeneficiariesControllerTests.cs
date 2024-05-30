dbd2d69d0d8eb947df87b74e39ba94e4303a2849
Faizal <fas.fazal@gmail.com>
2024-05-30 20:19:06 +0530

    test: Add unit tests for get beneficiaries API

---
+++ TopUpManager.API.Tests/Controllers/BeneficiariesControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using TopUpManager.API.Controllers;
+using TopUpManager.Common.Entity;
+using TopUpManager.Common.Interfaces.Services;
+
+namespace TopUpManager.API.Tests.Controllers
+{
+    public class BeneficiariesControllerTests
+    {
+        private readonly Mock<IBeneficiaryService> _mockBeneficiaryService;
+        private readonly BeneficiariesController _beneficiariesController;
+
+        public BeneficiariesControllerTests()
+        {
+            _mockBeneficiaryService = new Mock<IBeneficiaryService>();
+            _beneficiariesController = new BeneficiariesController(_mockBeneficiaryService.Object);
+        }
+
+        [Fact]
+        public async Task Get_ValidUserId_ReturnsOkResultWithBeneficiaries()
+        {
+            int userId = 1;
+            var expectedBeneficiaries = new List<Beneficiary>
+            {
+                new Beneficiary { Id = 1, Nickname = "Beneficiary 1", UserId = userId },
+                new Beneficiary { Id = 2, Nickname = "Beneficiary 2" ,UserId = userId },
+            };
+            _mockBeneficiaryService.Setup(service => service.GetBeneficiariesAsync(userId))
+                                   .ReturnsAsync(expectedBeneficiaries);
+
+            var result = await _beneficiariesController.Get(userId);
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnedBeneficiaries = Assert.IsAssignableFrom<List<Beneficiary>>(okResult.Value);
+            Assert.Equal(expectedBeneficiaries, returnedBeneficiaries);
+        }
+    }
+}
---
+++ TopUpManager.Services.Tests/Services/BeneficiaryServiceTests.cs
@@ -0,0 +1,50 @@
+﻿using Moq;
+using TopUpManager.Common.Entity;
+using TopUpManager.Common.Interfaces.DataAccess;
+using TopUpManager.Services.Services;
+
+namespace TopUpManager.Services.Tests.Services
+{
+    public class BeneficiaryServiceTests
+    {
+        private readonly Mock<IBeneficiaryRepo> _mockBeneficiaryRepo;
+        private readonly BeneficiaryService _beneficiaryService;
+
+        public BeneficiaryServiceTests()
+        {
+            _mockBeneficiaryRepo = new Mock<IBeneficiaryRepo>();
+            _beneficiaryService = new BeneficiaryService(_mockBeneficiaryRepo.Object);
+        }
+
+        [Fact]
+        public async Task GetBeneficiariesAsync_ReturnsListOfBeneficiaries()
+        {
+            int userId = 1;
+            var expectedBeneficiaries = new List<Beneficiary>
+            {
+                new Beneficiary { Id = 1, Nickname = "Beneficiary 1", UserId = userId },
+                new Beneficiary { Id = 2, Nickname = "Beneficiary 2" ,UserId = userId },
+            };
+            _mockBeneficiaryRepo.Setup(repo => repo.GetBeneficiariesAsync(userId))
+                               .ReturnsAsync(expectedBeneficiaries);
+
+            var result = await _beneficiaryService.GetBeneficiariesAsync(userId);
+
+            Assert.NotNull(result);
+            Assert.Equal(expectedBeneficiaries, result);
+        }
+
+        [Fact]
+        public async Task GetBeneficiariesAsync_ReturnsEmptyList_WhenNoBeneficiariesFound()
+        {
+            int userId = 1;
+            _mockBeneficiaryRepo.Setup(repo => repo.GetBeneficiariesAsync(userId))
+                               .ReturnsAsync(new List<Beneficiary>());
+
+            var result = await _beneficiaryService.GetBeneficiariesAsync(userId);
+
+            Assert.NotNull(result);
+            Assert.Empty(result);
+        }
+    }
+}

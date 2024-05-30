141b30cbecd50caba6287eafec7afa4decf9c897
Faizal <fas.fazal@gmail.com>
2024-05-30 09:44:37 +0530

    test: Add unit tests create user endpoint

---
+++ TopUpManager.API.Tests/Controllers/UsersControllerTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using TopUpManager.API.Controllers;
+using TopUpManager.Common.Entity;
+using TopUpManager.Common.Interfaces.Services;
+using TopUpManager.Common.Models.Request;
+
+namespace TopUpManager.API.Tests.Controllers
+{
+    public class UsersControllerTests
+    {
+        private readonly Mock<IUserService> _mockUserService;
+        private readonly UsersController _usersController;
+
+        public UsersControllerTests()
+        {
+            _mockUserService = new Mock<IUserService>();
+            _usersController = new UsersController(_mockUserService.Object);
+        }
+
+        [Fact]
+        public async Task Post_WithValidUserRequestModel_ShouldReturnOkResult()
+        {
+            var userModel = new UserRequestModel { Name = "Test User", IsVerified = true };
+            var user = new User { Id = 1, Name = "Test User", IsVerified = true };
+            _mockUserService.Setup(service => service.CreateUserAsync(userModel)).ReturnsAsync(user);
+
+            var result = await _usersController.Post(userModel);
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnedUser = Assert.IsType<User>(okResult.Value);
+            Assert.Equal(user.Id, returnedUser.Id);
+            Assert.Equal(userModel.Name, returnedUser.Name);
+            Assert.Equal(userModel.IsVerified, returnedUser.IsVerified);
+        }
+
+        [Fact]
+        public async Task Post_WithInvalidModelState_ShouldReturnBadRequest()
+        {
+            var userModel = new UserRequestModel { Name = "", IsVerified = true };
+            _usersController.ModelState.AddModelError("Name", "Name is required");
+
+            var result = await _usersController.Post(userModel);
+
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.IsType<SerializableError>(badRequestResult.Value);
+        }
+    }
+}
---
+++ TopUpManager.Services.Tests/Services/UserServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using Moq;
+using TopUpManager.Common.Entity;
+using TopUpManager.Common.Interfaces.DataAccess;
+using TopUpManager.Common.Models.Request;
+using TopUpManager.Services.Services;
+
+namespace TopUpManager.Services.Tests.Services
+{
+    public class UserServiceTests
+    {
+        private readonly Mock<IUserRepo> _mockUserRepo;
+        private readonly UserService _userService;
+
+        public UserServiceTests()
+        {
+            _mockUserRepo = new Mock<IUserRepo>();
+            _userService = new UserService(_mockUserRepo.Object);
+        }
+
+        [Fact]
+        public async Task CreateUserAsync_WithValidUserRequestModel_ShouldReturnNewUser()
+        {
+            var userModel = new UserRequestModel { Name = "Test User", IsVerified = true };
+
+            var result = await _userService.CreateUserAsync(userModel);
+
+            Assert.NotNull(result);
+            Assert.Equal(userModel.Name, result.Name);
+            Assert.Equal(userModel.IsVerified, result.IsVerified);
+            _mockUserRepo.Verify(repo => repo.AddUserAsync(It.IsAny<User>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task CreateUserAsync_WithNullUserRequestModel_ShouldThrowArgumentNullException()
+        {
+            // Arrange
+            UserRequestModel userModel = null;
+
+            // Act & Assert
+            await Assert.ThrowsAsync<ArgumentNullException>(() => _userService.CreateUserAsync(userModel));
+        }
+    }
+}

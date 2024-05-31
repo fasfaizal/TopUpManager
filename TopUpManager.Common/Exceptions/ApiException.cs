6ce2da1a014e7765fee88e76f8a4d2be111431d6
Faizal <fas.fazal@gmail.com>
2024-05-31 06:36:03 +0530

    chore add ApiException class to handle common exceptions

---
+++ TopUpManager.Common/Exceptions/ApiException.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace TopUpManager.Common.Exceptions
+{
+    public class ApiException : Exception
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public ApiException(HttpStatusCode statusCode, string message) : base(message)
+        {
+            StatusCode = statusCode;
+        }
+    }
+}

e9429d0be438b3608777877700039145b36e92d8
Faizal <fas.fazal@gmail.com>
2024-06-01 15:13:00 +0530

    chore: Add financial service mock API's

---
+++ FinanceService.API/Controllers/TransactionsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+        [HttpGet("balance")]
+        public IActionResult GetBalance()
+        {
+            // Implement get balance functionality
+            // In real the return value could be an object containing other fields as well
+            return Ok(100);
+        }
+
+        [HttpPost("debit")]
+        public IActionResult Debit()
+        {
+            // Implement debit functionality
+            return Ok();
+        }
+
+        [HttpPost("credit")]
+        public IActionResult Credit()
+        {
+            // Implement credit functionality
+            return Ok();
+        }
+    }
+}
